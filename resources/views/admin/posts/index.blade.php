6b5999d31c569f7ef6cd14dd1b662b1fbf04ea2d
AngeloLista <abstractplayer@gmail.com>
2022-03-29 17:32:53 +0200

    added categories in index view table

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -13,7 +13,7 @@
               <tr>
                 <th scope="col">#</th>
                 <th scope="col">Title</th>
-                <th scope="col">Slug</th>
+                <th scope="col">Category</th>
                 <th scope="col">Created at</th>
                 <th scope="col"></th>
               </tr>
@@ -25,7 +25,7 @@
                 <tr>
                     <th scope="row">{{ $post->id }}</th>
                     <td>{{ $post->title }}</td>
-                    <td>{{ $post->slug }}</td>
+                    <td>{{ $post->category->label }}</td>
                     <td>{{ $post->created_at }}</td>
                     <td class="d-flex justify-content-center align-items-center">
                       {{-- Details --}}
