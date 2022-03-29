eec559001a6f7543ce022efabc0a949ca72c18fc
AngeloLista <abstractplayer@gmail.com>
2022-03-29 17:15:19 +0200

    added categories in PostSeeder

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 
 use Faker\Generator as Faker;
 
@@ -16,8 +18,12 @@ class PostSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+
+        $category_ids = Category::pluck('id')->toArray();
+
         for($i = 0; $i < 53; $i++) {
             $post = new Post();
+            $post->category_id = Arr::random($category_ids);
             $post->title = $faker->text(50);
             $post->content = $faker->paragraphs(2, true);
             $post->image = $faker->imageUrl(250, 250);
