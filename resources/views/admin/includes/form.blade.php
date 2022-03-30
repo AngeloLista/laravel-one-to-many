22ee01d8d8445cb394129969b2faffe4af1e4957
AngeloLista <abstractplayer@gmail.com>
2022-03-30 16:58:56 +0200

    added display error in edit and create form

--- resources/views/admin/includes/form.blade.php
+++ resources/views/admin/includes/form.blade.php
@@ -19,33 +19,52 @@
             {{-- Title --}}
             <div class="col-8 mb-3 form-group">
                 <label for="title">Title: </label>
-                <input class="form-control" type="text" name="title" id="title" value="{{ old('title', $post->title) }}">
+                <input class="form-control @error('title') is-invalid @enderror" type="text" name="title" id="title" value="{{ old('title', $post->title) }}">
+                {{-- Display error --}}
+                @error('title')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                @enderror
             </div>
             {{-- Categories Select --}}
             <div class="col-4 mb-3 form-group">
                 <label for="category">Category: </label>
-                <select class="form-control" id="category_id" name="category_id">
+                <select class="form-control @error('category_id') is-invalid @enderror" id="category_id" name="category_id">
                     <option value="">No Category</option>
                     @foreach($categories as $category)
                     <option @if(old('category_id', $post->category_id) == $category->id) selected @endif value="{{ $category->id }}">{{ $category->label }}</option>
                     @endforeach
                 </select>
+                {{-- Display error --}}
+                @error('category_id')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                @enderror
             </div>
             {{-- Content --}}
             <div class="col-12 mb-3 form-group">
                 <div class="label">Content: </div>
-                <textarea class="form-control" type="text"
+                <textarea class="form-control @error('content') is-invalid @enderror" type="text"
                 rows="5"  name="content" id="content">{{ old('content', $post->content) }}</textarea>
+                {{-- Display error --}}
                 @error('content')
                     <div class="invalid-feedback">
-                        ASD
+                        {{$message}}
                     </div>
                 @enderror
             </div>
-
+            {{-- Image --}}
             <div class="col-12 mb-3 form-group">
                 <label for="image">Image Url: </label>
-                <input class="form-control" type="text" name="image" id="image" value="{{ old('image', $post->image) }}">
+                <input class="form-control @error('content') is-invalid @enderror" type="text" name="image" id="image" value="{{ old('image', $post->image) }}">
+                {{-- Display error --}}
+                @error('category_id')
+                    <div class="invalid-feedback">
+                        {{$message}}
+                    </div>
+                @enderror
             </div>
             
             <div class="d-flex justify-content-end col-12">
